63ed91884b9557cb6127b6ed8e0acdbfa45e9623
Sasha Glanville <51757958+sglanvil@users.noreply.github.com>
2022-05-05 15:59:03 -0600

    Create plot_map_areas.m

---
+++ plot_map_areas.m
@@ -0,0 +1,46 @@
+% May 5, 2022
+clear; clc; close all;
+
+load('coast.mat');
+latCoast=lat;
+lonCoast=long;
+
+figure;
+subplot('position',[.1 .2 .8 .6]);
+hold on; box on;
+plot(lonCoast,latCoast,'k','linewidth',1);
+set(gca,'ytick',-90:30:90,'yticklabel',{'90S' '60S' '30S' '0' '30N' '60N' '90N'});
+set(gca,'xtick',-180:90:180,'xticklabel',{'180W' '90W' '0' '90E' '180E'});
+xtickangle(45)
+axis([-180 180 -60 90]);
+
+% North America (lon>190 & lon<305,lat>15 & lat<75)
+x=[-170 -55 -55 -170]; % (deg east)-360
+y=[15 15 75 75];
+patch(x,y,'blue','facealpha',0.25,'linestyle','-');
+
+% South America 
+x=[-85 -35 -35 -85]; 
+y=[-20 -20 10 10];
+patch(x,y,'blue','facealpha',0.25,'linestyle','-');
+
+% Asia
+x=[60 145 145 60];
+y=[10 10 55 55];
+patch(x,y,'blue','facealpha',0.25,'linestyle','-');
+
+% Europe
+x=[-10 40 40 -10];
+y=[35 35 75 75];
+patch(x,y,'blue','facealpha',0.25,'linestyle','-');
+
+% Australia/SE Asia
+x=[95 180 180 95];
+y=[-50 -50 10 10];
+patch(x,y,'blue','facealpha',0.25,'linestyle','-');
+
+% Africa
+x=[-20 60 60 -20];
+y=[-10 -10 35 35];
+patch(x,y,'blue','facealpha',0.25,'linestyle','-');
+
