60047fcc2a5c9376d2f66ff243ef9876427c159d
Sasha Glanville <51757958+sglanvil@users.noreply.github.com>
2022-05-24 17:50:04 -0600

    Update prepare_NCEPCFSv2.sh

--- prepare_NCEPCFSv2.sh
+++ prepare_NCEPCFSv2.sh
@@ -3,23 +3,23 @@
 module load ncl
 
 ## copy files from rda
-for iyear in {2011..2022}; do
-        echo $iyear
-        cp /glade/collections/rda/data/ds094.0/$iyear/*.pgrbf.* /glade/scratch/sglanvil/NCEPCFSv2/
-done
+#for iyear in {2011..2022}; do
+#       echo $iyear
+#       cp /glade/collections/rda/data/ds094.0/$iyear/*.pgrbf.* /glade/scratch/sglanvil/NCEPCFSv2/ 
+#done
 
-## untar files
+## untar files (just using forecast = 1hr)
 for ifile in /glade/scratch/sglanvil/NCEPCFSv2/*.tar; do
-        echo $ifile
+        idate=$(sed -e 's/.*cdas1\.//' <<< $ifile | sed -e 's/\.pgrbf\.tar//')
+        echo $idate
         tar -xvf $ifile
-        rm $ifile
+        rename "pgrbf01" "pgrbf01.$idate" *.pgrbf01.grib2
+        rm *.pgrbf0[2-9].grib2
 done
-rm *.pgrbf0[2-9].*grib2
 
 ## convert from .grib2 to .nc
-for ifile in /glade/scratch/sglanvil/NCEPCFSv2/*.grib2; do
-        echo $ifile
-        ncl_convert2nc $ifile
-done
-rm *.grib2
+#for ifile in /glade/scratch/sglanvil/NCEPCFSv2/*.grib2; do
+        #echo $ifile
+        #ncl_convert2nc $ifile
+#done
 
