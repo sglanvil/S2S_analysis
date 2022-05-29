ed382f7d549a2c805af69b182f3e06ffd6fe641c
Sasha Glanville <51757958+sglanvil@users.noreply.github.com>
2022-05-29 16:54:53 -0600

    Update postproc_NCEPCFSR.sh

--- postproc_NCEPCFSR.sh
+++ postproc_NCEPCFSR.sh
@@ -18,9 +18,10 @@ while [[ $date -le $endDate ]]; do
         date=$(date -d "$date + 1 day" +"%Y%m%d")
         datePrevious=$(date -d "$date - 1 day" +"%Y%m%d")
         echo "Running t="$date"; t-1="$datePrevious 
-        size=$(du -shb T_NCEPCFSR.${date}.dailyAvg.nc | cut -f1)
-        echo "Size of T file="$size
-        if [[ ! -f T_NCEPCFSR.${date}.dailyAvg.nc || $size -lt 38000000 ]]; then
+        size=$(du -shb pr_sfc_NCEPCFSR.${date}.dailyAvg.nc | cut -f1)
+        echo "Size of last variable file="$size 
+        # check if last variable file exists (in this case, pr_sfc)
+        if [[ ! -f pr_sfc_NCEPCFSR.${date}.dailyAvg.nc || $size -lt 1046000 ]]; then
                 ncecat -O pgbh06.gdas.${datePrevious}18.nc pgbh06.gdas.${date}00.nc pgbh06.gdas.${date}06.nc pgbh06.gdas.${date}12.nc pgbh06.gdas.${date}.6hrly.nc
                 ncwa -O -a record pgbh06.gdas.${date}.6hrly.nc pgbh06.gdas.${date}.dailyAvg.nc
                 # ----- Temperature (lon,lat,p) = TMP_P0_L100_GLL0 (K)
@@ -34,7 +35,8 @@ while [[ $date -le $endDate ]]; do
 done
 
 # concatenate all date files, into single time series, making time the new record dimension
-ncecat -O -u time T_NCEPCFSR.[0-9]*.dailyAvg.nc T_NCEPCFSR.ALL.dailyAvg.nc
-ncecat -O -u time tas_2m_NCEPCFSR.[0-9]*.dailyAvg.nc tas_2m_NCEPCFSR.ALL.dailyAvg.nc
-ncecat -O -u time pr_sfc_NCEPCFSR.[0-9]*.dailyAvg.nc pr_sfc_NCEPCFSR.ALL.dailyAvg.nc
+# ncecat -O -u time T_NCEPCFSR.[0-9]*.dailyAvg.nc T_NCEPCFSR.ALL.dailyAvg.nc
+# ncecat -O -u time tas_2m_NCEPCFSR.[0-9]*.dailyAvg.nc tas_2m_NCEPCFSR.ALL.dailyAvg.nc
+# ncecat -O -u time pr_sfc_NCEPCFSR.[0-9]*.dailyAvg.nc pr_sfc_NCEPCFSR.ALL.dailyAvg.nc
+
 
