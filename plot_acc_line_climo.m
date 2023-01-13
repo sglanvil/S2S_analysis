c37c3e7f02a9bd04844e5694fa2269e50368c3a1
sglanvil <51757958+sglanvil@users.noreply.github.com>
2023-01-13 13:47:24 -0700

    Update plot_acc_line_climo.m

--- plot_acc_line_climo.m
+++ plot_acc_line_climo.m
@@ -3,17 +3,13 @@
 clear; clc; close all;
 
 % ------------------------- SPECIFY BELOW -------------------------
-var='tas_2m';
-obsName='ERA5';
-timeAvg='daily';
-season='JJA';
-scenarioName='scenario1';
-
-titleName='JJA 2m Temperature ACC (ERA5)';
-
-printName=...
-    sprintf('%s_ACC_%s_line_%sseason_ALLzones_style3_figure',...
-    var,obsName,season);
+% var='tas_2m'; varLong='2m Temperature'; obsName='ERA5';
+var='pr_sfc'; varLong='Surface Precipitation'; obsName='GPCP';
+composite='LA'; % [ALL,DJF,JJA,EL,LA]
+timeFreq='dailySmooth'; % [daily,dailySmooth]
+scenarioName='scenario1'; % [scenario1] only
+titleName=sprintf('%s %s ACC (%s)',composite,varLong,obsName);
+printName=sprintf('%s_ACC_line_%scomposite_%s_figure',var,composite,obsName);
 % ------------------------- SPECIFY ABOVE -------------------------
 
 lon=0:359;
@@ -32,7 +28,7 @@ mask=interp2(x,y,double(mask0)',xNew,yNew,'linear',1)';
 simList={'cesm2cam6climoATMv2','cesm2cam6climoLNDv2','cesm2cam6climoOCNv2',...
     'cesm2cam6v2','cesm2cam6climoOCNclimoATMv2','cesm2cam6climoALLv2',...
     'cesm2cam6climoOCNFIXclimoLNDv2','cesm2cam6climoALLFIXv2' };
-lineColor=[255 165 0; 34 139 34; 0 0 205; 0 0 0; 255 0 0]./255; % sim color
+lineColor=[204 187 68; 34 136 51; 102 204 238; 0 0 0]./255;
 
 zoneA={'lon>190 & lon<305','lat>15 & lat<75'}; % North America (lon>190 & lon<305,lat>15 & lat<75)
 zoneB={'lon>275 & lon<330','lat>-20 & lat<15'}; % South America (lon>275 & lon<330,lat>-20 & lat<15)
@@ -48,12 +44,20 @@ subpos=[0.05 0.65 0.2 0.2; 0.05 0.3 0.2 0.2; ...
         0.29 0.65 0.2 0.2; 0.29 0.3 0.2 0.2; ...
         0.53 0.65 0.2 0.2; 0.53 0.3 0.2 0.2; ...
         0.77 0.65 0.2 0.2];
+    
 
 for izone=1:7
     for isim=1:length(simList)
         simName=simList{isim};
-        accFile=sprintf('/Users/sglanvil/Documents/S2S_climo_experiments/ACC_daily/%s_ACC_%s_%sseason_%s_%s.%s_s2s_data.nc',...
-            var,obsName,season,timeAvg,simName,scenarioName);
+        
+        addpath /Users/sglanvil/Documents/S2S_climo_experiments/ACC_final/
+        sourceDir='/Users/sglanvil/Documents/S2S_climo_experiments/ACC_final/';
+        fileString1=sprintf('%s_ACC_%scomposite_%s_%s.scenario1_*sample_%s_s2s_data.nc',var,composite,timeFreq,simName,obsName);
+        accFile=dir(fullfile(sourceDir,fileString1)).name;     
+
+%         accFile=sprintf('/Users/sglanvil/Documents/S2S_climo_experiments/ACC_precip/%s_ACC_%s_%sseason_%s_%s.%s_s2s_data.nc',...
+%             var,obsName,composite,timeFreq,simName,scenarioName);
+
         lon=ncread(accFile,'lon');
         lat=ncread(accFile,'lat');
         acc=ncread(accFile,'ACC');
@@ -97,87 +101,89 @@ varAL=lndVar2-lndVar;
 varAO=ocnVar2-ocnVar;
 xxx=atmVar+varAL+varAO+lndVar+ocnVar+allClim;
 
+if strcmp(var,'pr_sfc')==1
+    for izone=1:7
+        for itime=1:46
+            if isinf(atmVar(izone,itime)) || isnan(atmVar(izone,itime))
+                atmVar(izone,itime)=standard(izone,itime);
+            end
+            if isinf(atmVar2(izone,itime)) || isnan(atmVar2(izone,itime))
+                atmVar2(izone,itime)=standard(izone,itime);
+            end
+        end
+    end
+end
+
 figure
 for izone=1:7
     subplot('position',subpos(izone,:))
     hold on; grid on; box on;
-    area([14 28],[1 1],'edgecolor','none','facecolor',[128 0 0]/255,...
+    area([14 28],[1 1],'edgecolor','none','facecolor',[120 120 120]/255,...
         'facealpha',0.1);
-    area([14 28],[-1 -1],'edgecolor','none','facecolor',[128 0 0]/255,...
+    area([14 28],[-1 -1],'edgecolor','none','facecolor',[120 120 120]/255,...
         'facealpha',0.1);
 
     plot(1:46,standard(izone,:),'color',lineColor(4,:),'linewidth',2.5)
 
     area(1:46,lndVar(izone,:),'edgecolor','none','facecolor',lineColor(2,:),...
-        'facealpha',0.2,'linewidth',1.5); 
-    plot(1:46,lndVar(izone,:),'color',lineColor(2,:),'linewidth',1.5);
+        'facealpha',0.2,'linewidth',2.5); 
+    plot(1:46,lndVar(izone,:),'color',lineColor(2,:),'linewidth',2.5);
     area(1:46,atmVar(izone,:),'edgecolor','none','facecolor',lineColor(1,:),...
-        'facealpha',0.2,'linewidth',1.5);
-    plot(1:46,atmVar(izone,:),'color',lineColor(1,:),'linewidth',1.5);
+        'facealpha',0.2,'linewidth',2.5);
+    plot(1:46,atmVar(izone,:),'color',lineColor(1,:),'linewidth',2.5);
     area(1:46,ocnVar(izone,:),'edgecolor','none','facecolor',lineColor(3,:),...
-        'facealpha',0.2,'linewidth',1.5);   
-    plot(1:46,ocnVar(izone,:),'color',lineColor(3,:),'linewidth',1.5);
+        'facealpha',0.2,'linewidth',2.5);   
+    plot(1:46,ocnVar(izone,:),'color',lineColor(3,:),'linewidth',2.5);
+    
     area(1:46,lndVar2(izone,:),'edgecolor','none','facecolor',lineColor(2,:),...
-        'facealpha',0.2,'linewidth',2,'linestyle',':');        
-    plot(1:46,lndVar2(izone,:),'color',lineColor(2,:),'linewidth',2,'linestyle',':');
+        'facealpha',0.2,'linewidth',2.5,'linestyle',':');        
+    plot(1:46,lndVar2(izone,:),'color',lineColor(2,:),'linewidth',2.5,'linestyle',':');
     area(1:46,atmVar2(izone,:),'edgecolor','none','facecolor',lineColor(1,:),...
-        'facealpha',0.2,'linewidth',2,'linestyle',':');     
-    plot(1:46,atmVar2(izone,:),'color',lineColor(1,:),'linewidth',2,'linestyle',':');
+        'facealpha',0.2,'linewidth',2.5,'linestyle',':');     
+    plot(1:46,atmVar2(izone,:),'color',lineColor(1,:),'linewidth',2.5,'linestyle',':');
     area(1:46,ocnVar2(izone,:),'edgecolor','none','facecolor',lineColor(3,:),...
-        'facealpha',0.2,'linewidth',2,'linestyle',':');        
-    plot(1:46,ocnVar2(izone,:),'color',lineColor(3,:),'linewidth',2,'linestyle',':');
+        'facealpha',0.2,'linewidth',2.5,'linestyle',':');        
+    plot(1:46,ocnVar2(izone,:),'color',lineColor(3,:),'linewidth',2.5,'linestyle',':');
     
-    plot(1:46,varAL(izone,:),'color',lineColor(2,:),'linewidth',2,'linestyle','--');
-    plot(1:46,varAO(izone,:),'color',lineColor(3,:),'linewidth',2,'linestyle','--');
-    plot(1:46,xxx(izone,:),'color','r','linewidth',2);
+%     plot(1:46,varAL(izone,:),'color',lineColor(2,:),'linewidth',2.5,'linestyle','--');
+%     plot(1:46,varAO(izone,:),'color',lineColor(3,:),'linewidth',2.5,'linestyle','--');
+%     plot(1:46,xxx(izone,:),'color',[187 187 187]./255,'linewidth',2.5,'linestyle','--');
 
-%     plot(1:46,climoLND(izone,:),'color',lineColor(2,:),'linewidth',2);        
-%     plot(1:46,climoATM(izone,:),'color',lineColor(1,:),'linewidth',2);
-%     plot(1:46,climoOCN(izone,:),'color',lineColor(3,:),'linewidth',2);    
-%     plot(1:46,lndFULL(izone,:),'color',lineColor(2,:),'linewidth',2,'linestyle',':');        
-%     plot(1:46,atmFULL(izone,:),'color',lineColor(1,:),'linewidth',2,'linestyle',':');     
-%     plot(1:46,ocnFULL(izone,:),'color',lineColor(3,:),'linewidth',2,'linestyle',':');  
-    
     xlabel('Week');
     title(zoneName{izone});
     axis([1.01 45 -0.1 0.9]); % do x=1.01 because "area" function plots y=0 at beginning
+    if strcmp(var,'pr_sfc')==1
+        axis([1.01 45 -0.1 0.6]); 
+        if izone<7
+            axis([14 45 -0.08 0.16]);  
+        end
+    end
     set(gca,'xtick',0:7:70,'xticklabel',0:1:7);
     set(gca,'ytick',0:0.2:1);
+    if strcmp(var,'pr_sfc')==1
+        set(gca,'ytick',0:0.1:1);
+    end
     set(gca,'fontsize',12);
 end
 
 p(1)=plot([1 45],[-100 -100],'color',lineColor(4,:),'linewidth',2.5);
-p(2)=plot([1 45],[-100 -100],'color',lineColor(1,:),'linewidth',2); 
-p(3)=plot([1 45],[-100 -100],'color',lineColor(2,:),'linewidth',2); 
-p(4)=plot([1 45],[-100 -100],'color',lineColor(3,:),'linewidth',2);
-p(5)=plot([1 45],[-100 -100],'color',lineColor(2,:),'linewidth',2,'linestyle','--');
-p(6)=plot([1 45],[-100 -100],'color',lineColor(3,:),'linewidth',2,'linestyle','--');
-p(7)=plot([1 45],[-100 -100],'color','r','linewidth',2);
+p(2)=plot([1 45],[-100 -100],'color',lineColor(1,:),'linewidth',2.5); 
+p(3)=plot([1 45],[-100 -100],'color',lineColor(2,:),'linewidth',2.5); 
+p(4)=plot([1 45],[-100 -100],'color',lineColor(3,:),'linewidth',2.5);
+% p(5)=plot([1 45],[-100 -100],'color',lineColor(2,:),'linewidth',2.5,'linestyle','--');
+% p(6)=plot([1 45],[-100 -100],'color',lineColor(3,:),'linewidth',2.5,'linestyle','--');
+% p(7)=plot([1 45],[-100 -100],'color',[187 187 187]./255,'linewidth',2.5,'linestyle','--');
 legend(p,'standard','atmosphere','land',...
-    'ocean','atmos-land','atmos-ocean','sum','box','off','position',[.77 .3 .2 .2],'fontsize',12);
+    'ocean','box','off','position',[.77 .3 .2 .2],'fontsize',12);
+% legend(p,'standard','atmosphere','land',...
+%     'ocean','atmos-land','atmos-ocean','sum','box','off','position',[.77 .3 .2 .2],'fontsize',12);
+
 annotation('textbox',[.04 .13 .8 .1],'string','\bfMethod 1: standard - climoX (solid)',...
     'edgecolor','none','verticalalignment','bottom','fontsize',12);
 annotation('textbox',[.04 .08 .8 .1],'string','\bfMethod 2: climoYclimoZ - climoALL (dotted)',...
     'edgecolor','none','verticalalignment','bottom','fontsize',12);
-annotation('textbox',[.04 .01 .8 .1],'string','\bfSum: climoALL + V_A + V_L + V_O + I_{AL} + I_{AO}',...
-    'edgecolor','none','verticalalignment','bottom','fontsize',12);
-
-% p(1)=plot([1 45],[-100 -100],'color',lineColor(4,:),'linewidth',2.5);
-% p(2)=plot([1 45],[-100 -100],'color',lineColor(1,:),'linewidth',2); 
-% p(3)=plot([1 45],[-100 -100],'color',lineColor(2,:),'linewidth',2); 
-% p(4)=plot([1 45],[-100 -100],'color',lineColor(3,:),'linewidth',2);
-% p(5)=plot([1 45],[-100 -100],'color',lineColor(1,:),'linewidth',2,'linestyle',':'); 
-% p(6)=plot([1 45],[-100 -100],'color',lineColor(2,:),'linewidth',2,'linestyle',':'); 
-% p(7)=plot([1 45],[-100 -100],'color',lineColor(3,:),'linewidth',2,'linestyle',':');
-% legend(p,'standard','atmosphere','land',...
-%     'ocean','box','off','position',[.77 .3 .2 .2],'fontsize',12);
-% legend(p,'standard','climoATM','climoLND',...
-%     'climoOCN','climoOCNclimoLND','climoOCNclimoATM','climoATMclimoLND',...
-%     'box','off','position',[.77 .3 .2 .2],'fontsize',12);
-% annotation('textbox',[.04 .1 .8 .1],'string','\bfMethod 1: climoX (solid)',...
-%     'edgecolor','none','verticalalignment','bottom','fontsize',12);
-% annotation('textbox',[.04 .05 .8 .1],'string','\bfMethod 2: climoYclimoZ (dashed)',...
+% annotation('textbox',[.04 .01 .8 .1],'string','\bfSum: climoALL + V_A + V_L + V_O + I_{AL} + I_{AO}',...
 %     'edgecolor','none','verticalalignment','bottom','fontsize',12);
 
 sgtitle(titleName,'fontweight','bold') 
-print(printName,'-r300','-dpng');
+% print(printName,'-r300','-dpng');
