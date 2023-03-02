a62a8f941601157a7323468e48836ccff9bc3d9b
sglanvil <51757958+sglanvil@users.noreply.github.com>
2023-03-02 09:07:31 -0800

    Update plot_acc_map_climo_ENSO.m

--- plot_acc_map_climo_ENSO.m
+++ plot_acc_map_climo_ENSO.m
@@ -12,8 +12,8 @@ clear; clc; close all;
 var='tas_2m'; varLong='2m Temperature'; obsName='ERA5';
 % var='pr_sfc'; varLong='Surface Precipitation'; obsName='GPCP';
 timeFreq='twoWeek'; % [twoWeek] only
-titleName=sprintf('ENSO Active - Neutral (ALL seasons) %s ACC',varLong);
-printName=sprintf('%s_ACC_map_specialENSO_style3_%s_figure',var,obsName);
+titleName=sprintf('Active - Neutral (ALL seasons) %s ACC',varLong);
+printName=sprintf('%s_ACC_map_ACTIVEminusNEUTRAL_ENSO_finalPaper_figure',var);
 % ------------------------- SPECIFY ABOVE -------------------------
 
 addpath /Users/sglanvil/Documents/S2S/     
@@ -34,11 +34,8 @@ lon=ncread(fil,'lon');
 lat=ncread(fil,'lat');
 lonNew=linspace(-180,180,length(lon)); 
 
-subpos1=[.07 .68 .22 .18; .07 .45 .22 .18; .07 .22 .22 .18];    
-subpos2=[.30 .68 .22 .18; .30 .45 .22 .18; .30 .22 .22 .18];    
-subpos3=[.53 .68 .22 .18; .53 .45 .22 .18; .53 .22 .22 .18];
-subpos4=[.76 .68 .22 .18; .76 .45 .22 .18; .76 .22 .22 .18];
-subpos=cat(3,subpos1,subpos2,subpos3,subpos4);
+subpos=[.22 .72 .34 .22; .22 .45 .34 .22; .22 .18 .34 .22];    
+
 
 subpos(:,1,:)=subpos(:,1,:)+0.12;
 
@@ -73,47 +70,51 @@ ACC1_biweekly=ACC1_active-ACC1_neutral;
 ACC2_biweekly=ACC2_active-ACC2_neutral;
 ACC3_biweekly=ACC3_active-ACC3_neutral;
 ACC_save=cat(4,ACC1_biweekly,ACC2_biweekly,ACC3_biweekly);
-titleNames={'standard','climoOCN','climoATMclimoLND'};
+% titleNames={'standard','climoOCN','climoATMclimoLND'};
+titleNames={'Active - Neutral ENSO'};
+subtitleALL={'(a)' '(b)' '(c)'};
 
 figure
 for iweek=1:3
-    for icase=1:3
-        ax(icase)=subplot('position',subpos(iweek,:,icase));
+    for icase=1
+        ax(icase)=subplot('position',subpos(iweek,:));
         hold on; box on;   
 
         ACC=squeeze(ACC_save(:,:,iweek,icase));
         ACCnew=[ACC(lon>=180 & lon<=360,:); ACC(lon>=0 & lon<180,:)];
         ACCnew(181,:)=ACCnew(182,:);
 %         ------ plot significance (ttest for correlation) ------
-        n=1000;
-        t=ACCnew./sqrt((1-ACCnew.^2)/(n-2));
+        n=500;
+        t=abs(ACCnew)./sqrt((1-ACCnew.^2)/(n-2));
         p=tcdf(t,n-2);        
         ACCnew(p<0.975)=NaN;
 
-        pcolor(lonNew,lat,ACCnew'); shading flat;
+        contourf(lonNew,lat,ACCnew',-1:0.1:1,'linestyle','none'); 
+        clim([-1 1]); colormap(gradsmap)
         plot(lonCoast,latCoast,'k','linewidth',1);
-        clim([-1 1]); colormap(gradsmap);
 
         set(gca,'box','on','layer','top');
         set(gca,'xtick',-180:90:180,'xticklabel',[]);
         set(gca,'ytick',-90:30:90,'yticklabel',[]);        
-        set(gca,'fontsize',9);
         if icase==1
             ylabel(weekNames{iweek},'fontweight','bold');
-            set(gca,'ytick',-90:30:90,'yticklabel',{'90S' '60S' '30S' '0' '30N' '60N' '90N'},'fontsize',9);
+            set(gca,'ytick',-90:30:90,'yticklabel',{'90S' '60S' '30S' '0' '30N' '60N' '90N'},'fontsize',10);
         end
         if iweek==1
-            title(titleNames{icase},'fontweight','bold','fontsize',10);
+            title(titleNames{icase},'fontweight','bold','fontsize',13);
         end
         if iweek==3
-            set(gca,'xtick',-180:90:180,'xticklabel',{'180W' '90W' '0' '90E' ' '},'fontsize',9);
+            set(gca,'xtick',-180:90:180,'xticklabel',{'180W' '90W' '0' '90E' '180E'},'fontsize',10);
             xtickangle(45)
         end       
         axis([-180 180 -60 90]);
     end
+    textboxdim=[.2 .5 .3 .3];
+    annotation('textbox',textboxdim,'String',subtitleALL{iweek},'Position',...
+        gca().Position,'Vert','bottom','fontweight','bold','fontsize',12);
 end
-cb=colorbar('location','southoutside','position',[.30 .1 .45 .03]);
-set(cb,'xtick',-1:0.2:1); ylabel(cb,'\bfACC','fontsize',10);
+cb=colorbar('location','southoutside','position',[.30 .08 .42 .03]);
+set(cb,'xtick',-1:0.2:1,'fontsize',8); ylabel(cb,'\bfACC','fontsize',10);
 set(gcf,'renderer','painters')
-sgtitle(titleName,'fontweight','bold') 
-% print(printName,'-r300','-dpng');
+% sgtitle('Active - Neutral ENSO','fontweight','bold') 
+print(printName,'-r300','-dpng');
